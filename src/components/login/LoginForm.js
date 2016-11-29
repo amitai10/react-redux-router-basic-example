eb7a0482265e4d81549f82466b0ef046d1f896bb
Amitai Barnea <amitai@spectory.com>
2016-11-29 11:14:10 +0200

    Aa

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import { browserHistory } from 'react-router'; 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-
 import InputField from '../common/InputField';
 import * as loginActions from '../../actions/loginActions';
 
